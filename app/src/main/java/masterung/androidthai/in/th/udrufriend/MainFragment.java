f672130c38a708ea8bc631c11e6b1330e0ea443c
masterUNG <phrombutr@gmail.com>
2018-11-19 15:32:50 +0700

    Replace Fragment Success

--- app/src/main/java/masterung/androidthai/in/th/udrufriend/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/udrufriend/MainFragment.java
@@ -2,10 +2,12 @@ package masterung.androidthai.in.th.udrufriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,10 +15,32 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        TextView textView = getView().findViewById(R.id.textViewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+
+    }   // Main Method
 
     public MainFragment() {
         // Required empty public constructor
-    }
+    }   // Constructor
 
 
     @Override
@@ -24,6 +48,6 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+    }   // Create View
 
-}
+}   // Main Class
