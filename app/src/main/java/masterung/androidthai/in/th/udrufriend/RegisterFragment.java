b61be2856cd18fef0cbb117dd8286f244afd363c
masterUNG <phrombutr@gmail.com>
2018-11-19 16:38:15 +0700

    Finish 19nov

--- app/src/main/java/masterung/androidthai/in/th/udrufriend/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/udrufriend/RegisterFragment.java
@@ -2,7 +2,9 @@ package masterung.androidthai.in.th.udrufriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,22 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.message_have_space);
+    }
 
     public RegisterFragment() {
         // Required empty public constructor
