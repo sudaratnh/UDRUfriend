5d95ff914a8d5d767fa4cc7d03c583dbe2188d88
masterUNG <phrombutr@gmail.com>
2018-11-20 11:26:14 +0700

    Get Image OK

--- app/src/main/java/masterung/androidthai/in/th/udrufriend/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/udrufriend/RegisterFragment.java
@@ -2,6 +2,9 @@ package masterung.androidthai.in.th.udrufriend;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 /**
@@ -17,6 +21,9 @@ import android.widget.ImageView;
  */
 public class RegisterFragment extends Fragment {
 
+    //    Explicit
+    private Uri uri;
+    private ImageView imageView;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -31,15 +38,43 @@ public class RegisterFragment extends Fragment {
 
     }   // Main Method
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            Toast.makeText(getActivity(),
+                    "Please Choose Image",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }   // Result
+
     private void avataController() {
-        ImageView imageView = getView().findViewById(R.id.imageViewAvata);
+        imageView = getView().findViewById(R.id.imageViewAvata);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent, "Please Choose App and Image"), 5);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Please Choose App and Image"), 5);
 
             }
         });
