450ee55b37bb144bcdf0b5e64c16451fee16c854
sudarat <sudarat.nh@gmail.com>
2018-11-21 14:03:14 +0700

    Sign Out Success

--- app/src/main/java/masterung/androidthai/in/th/udrufriend/ServiceActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/udrufriend/ServiceActivity.java
@@ -1,7 +1,14 @@
 package masterung.androidthai.in.th.udrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -9,5 +16,41 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Create Toolbar
+        createToolbar();
+
+    }   //Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemExit) {
+            signOutService();
+        }
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    private void signOutService() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+        startActivity(new Intent(ServiceActivity.this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        getSupportActionBar().setSubtitle(firebaseUser.getDisplayName());
+
+
+    }
+}   //Main Class
